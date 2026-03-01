2f16e906b47cc3f49547baab6d2833172f84094f
kho91270 <150074394+kho91270@users.noreply.github.com>
2026-03-01 02:06:17 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,30 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './context/LanguageContext';
 import { HelmetProvider } from 'react-helmet-async';
 
-// On garde l'import 'menubar' en minuscule comme confirmé
 import Menubar from './components/ui/menubar'; 
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
 import Contact from './pages/Contact';
-import Legal from './pages/Legal';
-import Privacy from './pages/Privacy';
-// Importe les autres pages ici...
 
 function App() {
   return (
     <HelmetProvider>
       <LanguageProvider>
         <Router>
-          {/* Structure Flex pour que le Footer soit toujours en bas */}
-          <div className="flex flex-col min-h-screen bg-white">
+          {/* On s'assure que la div parente ne bloque pas le scroll */}
+          <div className="relative min-h-screen flex flex-col bg-white">
             <Menubar /> 
-            <main className="flex-grow">
+            <main className="flex-grow pt-0">
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/contact" element={<Contact />} />
-                <Route path="/legal" element={<Legal />} />
-                <Route path="/privacy" element={<Privacy />} />
                 <Route path="*" element={<Home />} />
               </Routes>
             </main>
