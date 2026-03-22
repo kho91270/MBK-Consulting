41c7d781b7013c1169d5f766e3c488ca0b5a7e62
kho91270 <150074394+kho91270@users.noreply.github.com>
2026-03-22 23:02:08 +0100

    Update ScrollToTop.jsx

--- frontend/src/components/ScrollToTop.jsx
+++ frontend/src/components/ScrollToTop.jsx
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom"; // Vérifiez bien cet import
 
-export default function ScrollToTop() {
+const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
+    // Cette commande force le navigateur à remonter tout en haut
     window.scrollTo(0, 0);
   }, [pathname]);
 
   return null;
-}
+};
+
+export default ScrollToTop;
