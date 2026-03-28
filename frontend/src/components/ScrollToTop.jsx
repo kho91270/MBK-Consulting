c23a5ad9713ec58815d8f00b7d229885a4f7bbf1
kho91270 <150074394+kho91270@users.noreply.github.com>
2026-03-29 00:10:36 +0100

    Update ScrollToTop.jsx

--- frontend/src/components/ScrollToTop.jsx
+++ frontend/src/components/ScrollToTop.jsx
@@ -1,16 +1,11 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    try {
-      window.scrollTo(0, 0);
-    } catch (error) {
-      // Sécurité pour les navigateurs anciens
-      document.documentElement.scrollTop = 0;
-    }
+    window.scrollTo(0, 0);
   }, [pathname]);
 
   return null;
